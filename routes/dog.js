a6d38ea40d655eac2dda35fb63f5f42f2e143907
BharathMopuru <95495923+BharathMopuru@users.noreply.github.com>
2022-10-31 23:54:14 -0500

    dog array is added

--- routes/dog.js
+++ routes/dog.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+class Dog {
+  constructor(dog_Name, dog_Price, dog_Breed){
+    this.dog_Name = dog_Name;
+    this.dog_Price = dog_Price;
+    this.dog_Breed = dog_Breed;
+  }
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dog', { title: 'Search Results Dog' });
+  let dog1 = new Dog('jack', 4000, "pug");
+  let dog2 = new Dog('doller', 3000, "Bulldog");
+  let dog3 = new Dog('gun', 3500, 'Retriever')
+  res.render('dog', { title: 'Search Results Dog', dog : [dog1, dog2, dog3] });
 });
 
 module.exports = router;
